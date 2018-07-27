4f9aecc7359150c9c74b484b4b0ae7f27d5cf7ba
Matthew Pirocchi <pirocchi@amazon.com>
2018-07-27 12:33:46 -0700

    Generate NuGet.config at code-generation time.

--- packages/aws-cdk-dotnet/generate.sh
+++ packages/aws-cdk-dotnet/generate.sh
@@ -5,6 +5,22 @@ rm -rf ./src
 mkdir ./src
 dotnet new sln -n Amazon.CDK -o ./src
 
+NUGET_JSONMODEL=$(node -e "console.log(path.dirname(require.resolve('jsii-dotnet-jsonmodel/package.json')))")
+NUGET_GENERATOR=$(node -e "console.log(path.dirname(require.resolve('jsii-dotnet-generator/package.json')))")
+NUGET_RUNTIME=$(node -e "console.log(path.dirname(require.resolve('jsii-dotnet-runtime/package.json')))")
+
+NUGET_CONFIG_CONTENT="<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<configuration>
+  <packageSources>
+    <add key=\"nuget.org\" value=\"https://api.nuget.org/v3/index.json\" protocolVersion=\"3\" />
+    <add key=\"local-jsonmodel\" value=\"$NUGET_JSONMODEL\" />
+    <add key=\"local-generator\" value=\"$NUGET_GENERATOR\" />
+    <add key=\"local-runtime\" value=\"$NUGET_RUNTIME\" />
+  </packageSources>
+</configuration>"
+echo $NUGET_CONFIG_CONTENT > ./src/NuGet.config
+
+
 AWS_CDK=../@aws-cdk
 echo "Searching $AWS_CDK for jsii packages..."
 for i in $( ls $AWS_CDK ); do
